6d1c68168c14e1bf04e0ffd75272e31f0f714645
ramziea <ramoozattar555@gmail.com>
2024-11-06 14:57:07 +0200

    sequrity cq

--- api/routes/cqRouter.js
+++ api/routes/cqRouter.js
@@ -1,9 +1,11 @@
 const cqController = require("../controllers/cqController");
   const authMiddlewers = require('./../middlewares/authMiddlewers');
+  const dynamicMiddleware = require('./../middlewares/dynamicMiddleware');
   const express = require("express");
   const router = express.Router();
   router.use(authMiddlewers.protect);
-  router.route("/").get(cqController.getAllcq).post(cqController.createcq);
+  //router.route("/").get(cqController.getAllcq).post(cqController.createcq);
+  router.route("/").get(cqController.getAllcq).post(authMiddlewers.restrictTo("admin","doctor"),dynamicMiddleware.addVarBody("doctor","userId"),cqController.createcq);
   router
     .route("/:id")
     .get(cqController.getcq)
